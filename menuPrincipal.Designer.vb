1bf8b34b3acf3363f61829ac6f5be967abfed1aa
Manuel <manueldocanto777@hotmail.com>
2020-06-15 21:09:42 -0300

    Creacion de formularios 15/6

--- menuPrincipal.Designer.vb
+++ menuPrincipal.Designer.vb
@@ -22,17 +22,117 @@ Partial Class menuPrincipal
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btnActivar = New System.Windows.Forms.Button()
+        Me.btnEliminar = New System.Windows.Forms.Button()
+        Me.btnCambPass = New System.Windows.Forms.Button()
+        Me.btnDesactivar = New System.Windows.Forms.Button()
+        Me.btnCambNom = New System.Windows.Forms.Button()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(217, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(148, 16)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Listado de usuarios:"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(44, 60)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(492, 283)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'btnActivar
+        '
+        Me.btnActivar.Location = New System.Drawing.Point(44, 349)
+        Me.btnActivar.Name = "btnActivar"
+        Me.btnActivar.Size = New System.Drawing.Size(94, 23)
+        Me.btnActivar.TabIndex = 2
+        Me.btnActivar.Text = "Activar"
+        Me.btnActivar.UseVisualStyleBackColor = True
+        '
+        'btnEliminar
+        '
+        Me.btnEliminar.Location = New System.Drawing.Point(442, 349)
+        Me.btnEliminar.Name = "btnEliminar"
+        Me.btnEliminar.Size = New System.Drawing.Size(94, 23)
+        Me.btnEliminar.TabIndex = 3
+        Me.btnEliminar.Text = "Eliminar"
+        Me.btnEliminar.UseVisualStyleBackColor = True
+        '
+        'btnCambPass
+        '
+        Me.btnCambPass.Location = New System.Drawing.Point(342, 349)
+        Me.btnCambPass.Name = "btnCambPass"
+        Me.btnCambPass.Size = New System.Drawing.Size(94, 23)
+        Me.btnCambPass.TabIndex = 4
+        Me.btnCambPass.Text = "Cambiar Pass"
+        Me.btnCambPass.UseVisualStyleBackColor = True
+        '
+        'btnDesactivar
+        '
+        Me.btnDesactivar.Location = New System.Drawing.Point(144, 349)
+        Me.btnDesactivar.Name = "btnDesactivar"
+        Me.btnDesactivar.Size = New System.Drawing.Size(94, 23)
+        Me.btnDesactivar.TabIndex = 5
+        Me.btnDesactivar.Text = "Desactivar"
+        Me.btnDesactivar.UseVisualStyleBackColor = True
+        '
+        'btnCambNom
+        '
+        Me.btnCambNom.Location = New System.Drawing.Point(244, 349)
+        Me.btnCambNom.Name = "btnCambNom"
+        Me.btnCambNom.Size = New System.Drawing.Size(94, 23)
+        Me.btnCambNom.TabIndex = 6
+        Me.btnCambNom.Text = "Cambiar Nom"
+        Me.btnCambNom.UseVisualStyleBackColor = True
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Location = New System.Drawing.Point(244, 378)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(94, 23)
+        Me.btnSalir.TabIndex = 7
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
         '
         'menuPrincipal
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.AppWorkspace
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(585, 411)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.btnCambNom)
+        Me.Controls.Add(Me.btnDesactivar)
+        Me.Controls.Add(Me.btnCambPass)
+        Me.Controls.Add(Me.btnEliminar)
+        Me.Controls.Add(Me.btnActivar)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Label1)
         Me.Name = "menuPrincipal"
         Me.Text = "menuPrincipal"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btnActivar As Button
+    Friend WithEvents btnEliminar As Button
+    Friend WithEvents btnCambPass As Button
+    Friend WithEvents btnDesactivar As Button
+    Friend WithEvents btnCambNom As Button
+    Friend WithEvents btnSalir As Button
 End Class
--- menuPrincipal.vb
+++ menuPrincipal.vb
@@ -1,3 +1,5 @@
 ﻿Public Class menuPrincipal
+    Private Sub MenuStrip1_ItemClicked(sender As Object, e As ToolStripItemClickedEventArgs) 
 
+    End Sub
 End Class
